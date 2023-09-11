8abc4b5a43478c809e7c39c495c70d17728312d1
Terry-LT <tlatfulla@gmail.com>
2023-09-11 09:19:46 +0500

    fifth commit-change README file

--- contactus/src/main/java/com/terrylt_tamerlan/contactus/contactus/EmailContactUsController.java
+++ contactus/src/main/java/com/terrylt_tamerlan/contactus/contactus/EmailContactUsController.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin()
 
 @AllArgsConstructor
 @RequestMapping(path = "api/v1/contactUs")
